2e898a52bbb06c2aab253c503031fa8e62000819
pilot0311 <pilot0938@gmail.com>
2023-09-01 16:51:32 +0900

    9/1-2

--- days08/SCOTT_230901.sql
+++ days08/SCOTT_230901.sql
@@ -160,8 +160,192 @@ PIVOT(COUNT(*)
  PIVOT( COUNT(*)
   FOR h_month IN ( 1,2,3,4,5,6,7,8,9,10,11,12 ) 
  )ORDER BY job ASC;
+ 
 --[11]
-SELECT dbms_random.value
-       , FLOOR( dbms_random.value( 100000,1000000 ) )
-       , dbms_random.string('L', 5)
-   FROM dual;
+SELECT FLOOR( dbms_random.value( 100000,1000000 ) )
+FROM dual;
+
+--[12]
+SELECT d.deptno
+     , LISTAGG( NVL(ename,'사원없음') , '/') WITHIN GROUP ( ORDER BY ename )AS ename    
+FROM emp e RIGHT JOIN dept d ON e.deptno = d.deptno
+GROUP BY d.deptno;
+
+--[13]
+SELECT MAX(sal),MIN(sal)
+FROM emp
+WHERE deptno = 30;
+
+SELECT deptno,MAX(sal),MIN(sal)
+FROM emp
+GROUP BY deptno
+HAVING deptno = 30;
+--[13-2]
+SELECT *
+FROM emp
+WHERE sal IN ((SELECT MAX(sal)FROM emp WHERE deptno = 30),(SELECT MIN(sal)FROM emp WHERE deptno = 30)) AND deptno =30;
+
+WITH t AS (
+SELECT MAX(sal)max,MIN(sal)min
+FROM emp
+WHERE deptno = 30
+)
+SELECT e.*
+FROM emp e,t
+WHERE sal IN(t.max,t.min) AND deptno = 30;
+
+--[14]
+SELECT *
+FROM(
+SELECT d.deptno,d.dname,COUNT(e.empno)사원수, RANK()OVER(ORDER BY COUNT(*) desc)rank
+FROM emp e RIGHT JOIN dept d ON e.deptno = d.deptno
+GROUP BY d.deptno,d.dname
+)t
+WHERE rank IN(1,(SELECT COUNT(*) FROM dept));
+--[1]
+WITH temp AS (
+ SELECT d.deptno,dname,COUNT(empno)cnt
+ FROM emp e, dept d
+ WHERE e.deptno(+) = d.deptno
+ GROUP BY d.deptno,dname
+)
+SELECT *
+FROM temp
+WHERE cnt IN((SELECT MAX(cnt)FROM temp),(SELECT MIN(cnt)FROM temp));
+--[2]
+WITH a AS (
+SELECT d.deptno,dname,COUNT(empno)cnt
+FROM emp e, dept d
+ WHERE e.deptno(+) = d.deptno
+ GROUP BY d.deptno,dname
+ ), b AS (
+ SELECT MIN(cnt)mincnt,MAX(cnt)maxcnt
+ FROM a
+ )
+ SELECT a.*
+ FROM a,b
+ WHERE a.cnt IN(b.maxcnt,b.mincnt);
+ 
+ --[3]분석 함수 : FIRST, LAST
+ -- 집계함수와 함께 사용되어 주어진 그룹에 대해 내부 적으로 순위를 매겨 결과를 산출
+ WITH a AS (
+ SELECT d.deptno,dname,COUNT(empno)cnt
+ FROM emp e, dept d
+ WHERE e.deptno(+) = d.deptno
+ GROUP BY d.deptno,dname
+ )
+ SELECT 
+    MIN(deptno)KEEP(DENSE_RANK FIRST ORDER BY cnt) AS deptno
+    ,MIN(cnt)
+    ,MAX(deptno)KEEP(DENSE_RANK LAST ORDER BY cnt) AS deptno
+    ,MAX(cnt)
+ FROM a;
+ 
+SELECT COUNT(*)
+FROM dept;
+
+-------------------------
+--[분석함수]: 테이블에 있는 행에 대해 특정 그룹별로 집계 값을 산출 할때 사용하는 함수
+1) ROW_NUMBER()
+2) RANK()
+3) DENSE_RANK()
+4) CUME_DIST(): 주어진 그룹에 대한 상대적인 누적 분포도 값을 반환
+                분포도값(비율): 0<실수<=1
+    예) 부서별 급여에 따른 누적 분포도 값 조회
+    SELECT deptno, ename, sal
+        , CUME_DIST()OVER(PARTITION BY deptno ORDER BY sal) dept_dist
+    FROM emp;
+5) PERCENT_RANK(): 해당 그룹 내의 백분위 순위
+                    0<= 실수 <=1
+                    백분위순위: 그룹 안에서 해당 로우(행)의 값보다 [작은 값의 비율]    
+    SELECT deptno, ename, sal
+        , PERCENT_RANK()OVER(PARTITION BY deptno ORDER BY sal) dept_per
+    FROM emp;
+    -- 비교
+    SELECT deptno, ename, sal
+        , PERCENT_RANK()OVER(PARTITION BY deptno ORDER BY sal) dept_per
+        , CUME_DIST()OVER(PARTITION BY deptno ORDER BY sal) dept_dist
+    FROM emp
+    WHERE deptno = 30;
+                PERCENT_RANK     CUME_DIST    
+30	JAMES	950	    0	0.1666666666666666666666666666666666666667 (1/6)
+30	WARD	1250	0.2(1/5)	0.5     (3/6)
+30	MARTIN	1250	0.2(1/5)	0.5
+30	TURNER	1500	0.6(3/5)	0.6666666666666666666666666666666666666667 (4/6)
+30	ALLEN	1600	0.8(4/5)	0.8333333333333333333333333333333333333333 (5/6)
+30	BLAKE	2850	1	1
+
+6) NTILE(expr): 파티션 별로 expr에 명시된 만큼 분할한 결과를 반환 하는 함수
+    분할 하는 수를 버킷(bucket)이라고 한다
+    
+    SELECT deptno,ename,sal
+        , NTILE(4)OVER (ORDER BY sal)ntiles
+    FROM emp;
+    
+    SELECT buseo,name,basicpay
+        , NTILE(2)OVER(PARTITION BY buseo ORDER BY basicpay)
+    FROM insa;
+    
+7) WIDTH_BUCKET(expr, min_value, max_value, num_bucket)
+    NTILE() 함수와 유사한 함수
+    차이점: (최소값, 최대값 설정가능)
+    예)
+    SELECT deptno, ename,sal
+        , NTILE(4)OVER (ORDER BY sal ASC)ntiles
+        , WIDTH_BUCKET(sal, 0, 5000, 4)withbuckets
+    FROM emp;
+    
+8) LAG(expr, offset, default_value)
+    : 주어진 그룹과 순서에 따라 다른 행에 있는 값을 참조 할 때 사용하는 함수
+    앞(선행 행)
+   LEAD(expr, offset, default_value)
+    : 주어진 그룹과 순서에 따라 다른 행에 있는 값을 참조 할 때 사용하는 함수
+    뒤 (후행 행)
+    SELECT ename,hiredate,sal
+        , LAG(sal,1,0)OVER(ORDER BY hiredate)AS prev_sal
+        , LAG(sal,2,-1)OVER(ORDER BY hiredate)AS prev_sal
+        , LEAD(sal,1,-1)OVER(ORDER BY hiredate)AS next_sal
+    FROM emp
+    WHERE deptno =30;
+
+--
+SELECT '***ADMIN***'
+    ,REPLACE('***ADMIN***','*','')
+    ,REPLACE('***AD**MIN***','*','')
+    ,TRIM('*' FROM '***AD**MIN***')
+    ,TRIM(' ' FROM '   AD**MIN   ')
+FROM dual;
+
+-- 오라클 자료형 --
+1) CHAR[size[BYTE|CHAR]]
+    CHAR == CHAR(1)
+    CHAR(10)
+    CHAR(10 byte)
+    CHAR(10 char)    
+    기본값 : 1
+    size크기의 고정길이 문자 데이터
+    1바이트 ~ 2000바이트
+    
+    이름 문자열: ename CHAR -> 1바이트라 3바이트인 한글 입력 불가
+                ename CHAR(10) -> 10바이트 -> 만약 3바이트 만사용해도 남은 크기는 공백으로 가지고있음
+                주민등록 번호같이 고정된 길이 입력시 사용
+                ename CHAR(10 char)-> 영어 한글 관계 없이 10[문자] 입력가능
+                
+create table test (aa char, bb char(3), cc char(3 char));
+DESC test;
+AA    CHAR(1)      --1바이트
+BB    CHAR(3)      --3바이트 
+CC    CHAR(3 CHAR) -- 문자 3개
+
+insert into test (aa,bb,cc) values('a','aaaa','aaaa'); --1바이트 4바이트 4바이트
+--ORA-12899: value too large for column "SCOTT"."TEST"."BB" (actual: 4, maximum: 3) bb칼럼 최대 크기 3인데 4바이트 입력함
+insert into test values('a','aaa','aaaa');
+-- ORA-12899: value too large for column "SCOTT"."TEST"."CC" (actual: 4, maximum: 3)cc칼럼 최대 크기 3문자인데 4문자 입력함
+insert into test values('a','aaa','aaa'); -- 정상 입력
+insert into test values('b','욜','우리'); -- 정상 입력
+insert into test values('c','우리','우리'); -- 한글은 한글자당 3바이트
+--ORA-12899: value too large for column "SCOTT"."TEST"."BB" (actual: 6, maximum: 3) bb칼럼 크기 3바이트 입력문자 6바이트
+
+COMMIT;
+SELECT *
+FROM test;
